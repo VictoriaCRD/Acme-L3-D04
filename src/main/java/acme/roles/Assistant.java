7487a7ea5526e727d0b92d3a459983aae2d443ea
marpedrod3 <marpedrod3@alum.us.es>
2023-02-28 17:39:19 +0100

    Creado assistant

---
+++ src/main/java/acme/roles/Assistant.java
@@ -0,0 +1,55 @@
+/*
+ * Consumer.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Assistant extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			supervisor;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			fields;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			resume;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
