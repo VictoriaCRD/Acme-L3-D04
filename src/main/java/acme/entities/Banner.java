36f8356ca641188dd18eac5cc469d86e88474756
Pablobi <pablo.bermudez.medina@gmail.com>
2023-03-01 21:42:27 +0100

    D02-T14 Done

---
+++ src/main/java/acme/entities/Banner.java
@@ -0,0 +1,47 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date		moment;
+
+	@NotBlank
+	@Length(max = 76)
+	protected String	slogan;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@PastOrPresent
+	@NotNull
+	protected Date		period;
+
+	@URL
+	@Transient
+	protected String	picture;
+
+	@URL
+	protected String	link;
+}
