5594bf0ee86fe8e9b4619ba38be9a672dcef131a
alvar <alvar@dircom.manten.us.es>
2023-03-02 16:14:59 +0100

    Individual Task004: Create role Company

---
+++ src/main/java/acme/roles/Company.java
@@ -0,0 +1,39 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Company extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			name;
+
+	@NotBlank
+	@Length(max = 26)
+	protected String			vatNumber;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			sumary;
+
+	@URL
+	protected String			link;
+}
