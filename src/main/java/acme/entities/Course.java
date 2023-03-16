6e16a0a2c7c8e94ab7afd53b65e17bf8557e64ea
marpedrod3 <marpedrod3@alum.us.es>
2023-03-16 18:20:59 +0100

    Cambios en el UML

--- src/main/java/acme/entities/Course.java
+++ src/main/java/acme/entities/Course.java
@@ -12,64 +12,15 @@
 
 package acme.entities;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.PositiveOrZero;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
-
-import acme.framework.data.AbstractRole;
+import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Course extends AbstractRole {
-
-	// Serialisation identifier -----------------------------------------------
-
-	protected static final long	serialVersionUID	= 1L;
-
-	// Attributes -------------------------------------------------------------
-
-	@NotBlank
-	@Column(unique = true)
-	@Pattern(regexp = "“[A-Z]{1,3}[0-9][0-9]{3}")
-	protected String			code;
-
-	@NotBlank
-	@Length(max = 75)
-	protected String			title;
-
-	@NotBlank
-	@Length(max = 100)
-	protected String			abstractm;
-
-	@NotBlank
-	@PositiveOrZero
-	protected Double			price;
-
-	@NotBlank
-	protected EnumType			sessionType;
-
-	@URL
-	protected String			link;
-
-	// Derived attributes -----------------------------------------------------
-
-	// Relationships ----------------------------------------------------------
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-
-	protected Tutorial			tutorial;
+public class Course extends AbstractEntity {
 
 }
