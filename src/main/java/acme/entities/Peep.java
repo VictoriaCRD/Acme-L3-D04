43e8985d18fe2c80b4e098d9f6302909f4b56482
alvaro <alvparbor1@alum.us.es>
2023-03-16 21:32:50 +0100

    Solución de errores

--- src/main/java/acme/entities/Peep.java
+++ src/main/java/acme/entities/Peep.java
@@ -1,25 +1,27 @@
+
 package acme.entities;
+
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
-import acme.roles.Company;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Peep extends AbstractEntity { 
+public class Peep extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
@@ -29,10 +31,10 @@ public class Peep extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Length(max=75)
+	@Length(max = 75)
 	protected String			title;
 
-    @Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
 	protected Date				startDate;
 
